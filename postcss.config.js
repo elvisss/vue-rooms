7fc98eaf1850da4b9ddd75206f9d705ff16b5e94
Elvis Saavedra <elvis.saavedra.segura@gmail.com>
2020-11-29 19:29:39 -0500

    first structure

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const postcssImport = require('postcss-import')
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  plugins: [postcssImport(), tailwindcss('./tailwind.js'), autoprefixer()],
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,17 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import HomePage from '../views/HomePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/',
+    name: 'HomePage',
+    component: HomePage,
   }
-];
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-});
+  routes,
+})
 
-export default router;
+export default router
